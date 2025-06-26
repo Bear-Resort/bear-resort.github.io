2b98224b431873204e53788852becd7991cc3f16
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-06-26 09:16:53 +0800

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -35,7 +35,7 @@ export function loginU(username, password) {
         localStorage.setItem("loggedInUser", username);
         localStorage.setItem("usrPasscode", password);
 
-        const rowUsr = users.find(row => row.name === usernameEnc);
+        const rowUsr = users.find(row => row.username === usernameEnc);
         const encData = rowUsr["enccode"];
         const pW = Number(encData);
         const encPW = stringToHash(password);
