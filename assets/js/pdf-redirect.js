82a4bb96e121591cea7141217699b014c0594c7e
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-06-28 14:57:37 +0800

    bugs fixed with current pdf renderer

--- assets/js/pdf-redirect.js
+++ assets/js/pdf-redirect.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const pdfLinks = document.querySelectorAll('a[href$=".pdf"]');  // Select all PDF links
+  const pdfLinks = document.querySelectorAll('a[href$=".pdf"]');  // Select all PDF links
 
-    // Attach click event listener to each PDF link
-    pdfLinks.forEach(link => {
+  // Attach click event listener to each PDF link
+  pdfLinks.forEach(link => {
     link.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent default link behavior
-        const pdfUrl = link.getAttribute('href'); // Get the href of the PDF link
+      event.preventDefault(); // Prevent default link behavior
+      const pdfUrl = link.getAttribute('href'); // Get the href of the PDF link
 
-        // Redirect to the view-pdf.html with the PDF URL as a query parameter
-        window.location.href = '/pdf-renderer.html?pdf=' + encodeURIComponent(pdfUrl);
+      // Open the PDF renderer in a new tab with the PDF URL as a query parameter
+      window.open('/pdf-renderer.html?pdf=' + encodeURIComponent(pdfUrl), '_blank');
     });
-    });
+  });
 });
