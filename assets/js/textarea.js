5a6a87fc9f36d8399a544673695e53d5da876c05
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-07-27 20:47:52 +0800

    bug fixed with typo

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -2,11 +2,11 @@ const textarea = document.querySelectorAll('textarea');
 
 function adjustHeight() {
     textarea.forEach(ta => {
-        ta.style.height = textarea.scrollHeight + 'px';
+        ta.style.height = ta.scrollHeight + 'px';
     })
 }
 
 textarea.forEach(ta => {
     ta.addEventListener('input', adjustHeight);
-    ta.style.height = textarea.scrollHeight + 'px';
+    ta.style.height = ta.scrollHeight + 'px';
 })
