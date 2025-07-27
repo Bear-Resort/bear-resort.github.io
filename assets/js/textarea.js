a75576de003664e5a0406f7788cea0ac12f1f82c
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-07-27 20:53:46 +0800

    Update textarea.js

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -2,11 +2,13 @@ const textarea = document.querySelectorAll('textarea');
 
 function adjustHeight() {
     textarea.forEach(ta => {
+        ta.style.height = 'auto';
         ta.style.height = ta.scrollHeight + 'px';
     })
 }
 
 textarea.forEach(ta => {
     ta.addEventListener('input', adjustHeight);
+    ta.style.height = 'auto';
     ta.style.height = ta.scrollHeight + 'px';
 })
