45ec51ff2e5802f2c718e28daff8be2807a86323
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-09-20 03:21:24 -0400

    add authrnticator

---
+++ authenticator/index.md
@@ -0,0 +1,13 @@
+---
+title: "Authenticator"
+---
+
+<div style="text-align: center">
+    <div id="result" style="display: none">
+    <h2><span class="eng">Your authenticator code is: </span><span class="chn">您的验证码是</span><code id="cc"></code></h2>
+    </div>
+    <div id="alert">
+        <span class="eng">Log in to get authenticator code.</span><span class="chn">登录以获得验证码。</span><br>
+        <button onclick="window.open('/assets/html/login.html')"><span class="eng">Log in</span><span class="chn">登录</span></button>
+    </div>
+</div>
---
+++ authenticator/main.js
@@ -0,0 +1,25 @@
+import { isLoggedIn } from "../assets/js/login";
+
+const authoDiv = document.getElementById("result");
+const alertDiv = document.getElementById("alert");
+const codeSpan = document.getElementById("cc");
+
+const rendercode = () => {
+    if (isLoggedIn) {
+        alertDiv.style.display = "none";
+        authoDiv.style.display = "block";
+        renderResult();
+    } else {
+        alertDiv.style.display = "block";
+        authoDiv.style.display = "none";
+        codeSpan.textContent = "";
+    }
+}
+
+const renderResult = () => {
+    const v1 = hashDeterministically(localStorage.getItem("loggedInUser")) % 1000;
+    const v2 = urlParams.get('auth') ** 13 % 100;
+    codeSpan.textContent = v1.toString() + v2.toString();
+}
+
+document.addEventListener("DOMContentLoaded", rendercode);
