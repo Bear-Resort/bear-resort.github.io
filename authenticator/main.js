4c892b37ba9de38a2f47d6a6a0a8619cb4d799db
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-09-20 03:31:28 -0400

    added parameter feature

--- authenticator/main.js
+++ authenticator/main.js
@@ -18,6 +18,7 @@ const rendercode = () => {
 
 const renderResult = () => {
     const v1 = hashDeterministically(localStorage.getItem("loggedInUser")) % 1000;
+    const urlParams = new URLSearchParams(window.location.search);
     const v2 = urlParams.get('auth') ** 13 % 100;
     codeSpan.textContent = v1.toString() + v2.toString();
 }
