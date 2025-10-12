7458edac23e531fef8823de70ddbc80d6e854cd8
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-10-12 00:23:48 -0400

    added form with questions

---
+++ authenticator/form.js
@@ -0,0 +1,67 @@
+import { isLoggedIn } from "/assets/js/login.js";
+
+const authoDiv = document.getElementById("result");
+const alertDiv = document.getElementById("alert");
+const authDiv = document.getElementById("no-auth");
+const response = document.getElementById("inputBox");
+const submitBtn = document.getElementById("submit");
+const quest = document.getElementById("question");
+
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwxVHgyxO8kUy84SgOZxuwC50uKHsnsMJCxq76VouA8I4pGoY_zVVEMDWvjoXx5w83LnQ/exec';
+
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const question = urlParams.get('quest');
+const usr = urlParams.get('usr').trim();
+
+
+const rendercode = () => {
+    if (isLoggedIn()) {
+        alertDiv.style.display = "none";
+        authoDiv.style.display = "block";
+        authDiv.style.display = "none";
+        renderResult();
+    } else if (localStorage.getItem("loggedInUser") !== usr) {
+        alertDiv.style.display = "none";
+        authoDiv.style.display = "none";
+        authDiv.style.display = "block";
+    } else {
+        alertDiv.style.display = "block";
+        authoDiv.style.display = "none";
+        authDiv.style.display = "none";
+        quest.innerText = question;
+    }
+}
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    submitBtn.disabled = true;
+
+    const formData = {
+        userId: localStorage.getItem("loggedInUser"),
+        formName: question,
+        response: response.value,
+    };
+
+    fetch(SCRIPT_URL, {
+    method: "POST",
+    mode: "no-cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData)
+    })
+    .then(res => res.text())
+    .then(msg => {
+    alert("✅ Success / 成功");
+    form.reset();
+    submitBtn.disabled = false;
+    })
+    .catch(err => {
+    alert("❌ Failed / 失败");
+    alert("🛜 This might be an issue of the internet / 也许是网络失效");
+    submitBtn.disabled = false;
+    });
+})
+
+document.addEventListener("DOMContentLoaded", rendercode);
---
+++ authenticator/form.md
@@ -0,0 +1,21 @@
+---
+title: "Private Form"
+---
+
+<div style="text-align: center">
+    <div id="result" style="display: none">
+        <h2 id="question"></h2>
+        <textarea id="inputBox" name="feedback" placeholder="Enter your reponse here... / 输入你的回答..." style="font-size: 16px; height: 39px;"></textarea>
+        <button id="submit"><span class="eng">Submit</span><span class="chn">提交</span></button>
+    </div>
+    <div id="alert">
+        <span class="eng">Log in to get authenticator code.</span><span class="chn">登录以获得验证码。</span><br>
+        <button onclick="window.open('/assets/html/login.html')"><span class="eng">Log in</span><span class="chn">登录</span></button>
+    </div>
+    <div id="no-auth">
+        <span class="eng">You are not authorized to fill in the form.</span><span class="chn">你无权填写本表格。</span><br>
+        <button onclick="window.open('/assets/html/login.html')"><span class="eng">Log in</span><span class="chn">登录</span></button>
+    </div>
+</div>
+
+<script type="module" src="./form.js"></script>
